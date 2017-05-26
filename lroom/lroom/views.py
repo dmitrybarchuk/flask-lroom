fa29322dd4bd6976bda10e2395762e345fd67613
Dmitry Barchuk <dmitry.barchuk@gmail.com>
2017-05-26 16:57:15 +0600

    routing

--- lroom/lroom/views.py
+++ lroom/lroom/views.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from lroom import app
 from models import Page
 
@@ -5,3 +7,8 @@ from models import Page
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+@app.route('/<page_slug>')
+def page(page_slug):
+    return render_template('page.html', page=Page.query.filter_by(slug=page_slug).first())
