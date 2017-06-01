ad1cee5d7fcff89681b0ba768274858d1c17cf88
dmitry.barchuk <dmitry.barchuk@gmail.com>
2017-06-01 09:33:11 +0600

    crud begining 404 error page

--- lroom/app/views.py
+++ lroom/app/views.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from flask import render_template
+from flask import render_template, request
 
 from app import app
 from app.models import Page
@@ -13,3 +13,15 @@ def index():
 @app.route('/<page_slug>')
 def page(page_slug):
     return render_template('page.html', page=Page.query.filter_by(slug=page_slug, published=True).first())
+
+
+@app.route('/<page_slug>/edit/', methods=['GET', 'POST'])
+def page_edit(page_slug):
+    page = Page.query.filter_by(slug=page_slug).first()
+    if page:
+        return render_template('page_form.html', page=page)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
--- lroom/run.py
+++ lroom/run.py
@@ -1,4 +1,5 @@
+import local_settings
 from app import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=local_settings.DEBUG)
